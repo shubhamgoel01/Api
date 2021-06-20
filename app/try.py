576c6415c86b31181f51890fd1083903d2b860b3
Shubham <thorgoyal@gmail.com>
2021-06-20 18:15:30 +0530

    2nd

--- app/try.py
+++ app/try.py
@@ -1,3 +1,11 @@
+# crucial = {'eggs': 1,'ham': 2,'cheese': 3}
+# dishes = {'eggs': 1, 'sausage': 1, 'bacon': 1, 'spam': 500}
+
+# for key in crucial.keys():
+#     if key in dishes.keys():
+#         print(dishes[key])
+
+
 a = [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
 b = [{'id': 1, 'body': 'some comment1', 'postId': 1}, {'id': 2, 'body': 'some comment2', 'postId': 1}]
 
@@ -14,28 +22,17 @@ for i in range(a_length):
             print('postId: ',b[i]['postId'],'\n\n')
 
 
-# <!-- {% for i in range(a_length) %}
-#     {% for j in range(b_length) %} 
-#         {% if(a[i]['id'] == b[j]['id']) %}
-#             {{ print(i,j) }} 
-#             {{ print("id: ",a[i]['id']) }}
-#             {{ print('title: ',a[i]['title']) }}
-#             {{ print('body: ',b[i]['body']) }}
-#             {{ print('postId: ',b[i]['postId'],'\n\n') }}
 
-
-#         {% endif %}
+# 1 {{ post_response }}<br>
+# 2{{ post_response.id }}<br>
+# 3 {{ comment_response.body }}<br>
+# 4 {{ length1 }}<br>
+# 5 {{ length2 }}<br>
+# 6   
+# {% for i in post_response %}
+# {% for j in comment_response %}
+#     {% for k in i %}
+#         {{ k }}
 #     {% endfor%}
-# {% endfor%} -->
-#     <!-- <h2>Post api result </h2>
-#     {% for i in post_response %}
-#     <b>{{ i }}</b></br>
-    
-#     {% endfor %}
-#     <hr>
-#     <h2>Comment api result </h2>
-    
-#     {% for i in comment_response %}
-#     <b>{{ i  }}</b></br>
-#     {% endfor %} -->
-
+# {% endfor%}
+# {% endfor%}
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.http import response
+from django.http import HttpResponse
 from django.shortcuts import render
 from apipro import urls
 import json
@@ -7,19 +7,29 @@ import requests
 def home(request):   
     response_post=requests.get('https://my-json-server.typicode.com/typicode/demo/posts').json()
     response_comment=requests.get('https://my-json-server.typicode.com/typicode/demo/comments').json()
+
     response_post_length = len(response_post)
     response_comment_length = len(response_comment)
  
+    response = []
 
-    context = {
-        'title': 'Shubham , lets start api',
-        'post_response':response_post,
-        'comment_response':response_comment,
-        'length1':response_post_length,
-        'length2':response_comment_length
-    }
-    return render(request,'app/home.html',context)
+    for a in range(response_post_length):
+        # Creating a list of comments
+        comments_of_post = []
+        for b in range(response_comment_length):
+            if response_post[a]["id"] == response_comment[b]["postId"]:
+                comments_of_post.append(response_comment[b])
+        # Appending it to post list
+        response_post[a]["comments"] = comments_of_post
+        response.append(response_post[a])
 
-    #  'response_post_length':response_post_length,
-    #     'response_comment_length':response_comment_length
-   
+    # print(response)
+
+    # context = {
+    #     'title': 'Shubham , lets start api',    
+    #     "response": response
+    # }
+    # return render(request,'app/home.html',context)
+
+    return HttpResponse(response)
+
